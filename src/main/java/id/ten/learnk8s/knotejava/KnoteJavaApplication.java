e0eddb6986620582fb40a8433325f2d50d417506
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-28 11:41:09 +0700

    first

---
+++ src/main/java/id/ten/learnk8s/knotejava/KnoteJavaApplication.java
@@ -0,0 +1,13 @@
+package id.ten.learnk8s.knotejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoteJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoteJavaApplication.class, args);
+	}
+
+}
